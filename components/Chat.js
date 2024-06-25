57ed5715933c9a53c1d3e1d0f6d1ca789ca72de5
Maggie-dev93 <139060168+Maggie-dev93@users.noreply.github.com>
2024-06-25 22:25:45 +0900

    5.2

--- components/Chat.js
+++ components/Chat.js
@@ -1,16 +1,73 @@
-import { useEffect } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { useEffect, useState } from 'react';
+import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
+import { Bubble, GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
+    // Extracting name and background from route params
     const { name, background } = route.params;
+    
+    // State to hold chat messages
+    const [messages, setMessages] = useState([]);
+
+    // Function to handle sending new messages
+    const onSend = (newMessages) => {
+      setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages));
+    }
+
+    // Custom bubble styles for chat messages
+    const renderBubble = (props) => {
+      return (
+        <Bubble
+          {...props}
+          wrapperStyle={{
+            right: {
+              backgroundColor: "#000"
+            },
+            left: {
+              backgroundColor: "#FFF"
+            }
+          }}
+        />
+      );
+    }
+
+    // useEffect to set navigation title and initial messages
+    useEffect(() => {
+        setMessages([
+          {
+            _id: 1,
+            text: "Hello developer",
+            createdAt: new Date(),
+            user: {
+              _id: 2,
+              name: "React Native",
+              avatar: "https://placeimg.com/140/140/any",
+            },
+          },
+          {
+            _id: 2,
+            text: 'This is a system message',
+            createdAt: new Date(),
+            system: true,
+          },
+        ]);
+    }, []);
 
     useEffect(() => {
-        navigation.setOptions({ title: name });
-    }, []);
+    navigation.setOptions({ title: name });
+  }, []);
 
     return (
         <View style={[styles.container, { backgroundColor: background }]}>
-            <Text>Welcome to the chat {name}!</Text>
+            <GiftedChat
+              messages={messages}
+              renderBubble={renderBubble}
+              onSend={messages => onSend(messages)}
+              user={{
+                _id: 1
+              }}
+            />
+            {Platform.OS === "ios" ? <KeyboardAvoidingView behavior="height" /> : null}
         </View>
     );
 }
@@ -18,8 +75,6 @@ const Chat = ({ route, navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
     }
 });
 
